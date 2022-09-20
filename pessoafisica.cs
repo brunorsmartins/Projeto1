eae62e8835c8bc41fa0ac79149e22235437cfd51
Bruno Martins <brunorsmartins@hotmail.com>
2022-09-20 16:13:28 -0300

    Commit teste

---
+++ pessoafisica.cs
@@ -0,0 +1,10 @@
+namespace Classes;
+
+    public class pessoafisica
+    {
+        public string nome;
+        public string cpf;
+        public int idade;
+        
+        
+    }
---
+++ pessoajuridica.cs
@@ -0,0 +1,9 @@
+namespace Classes
+{
+    public class pessoajuridica
+    {
+        public string rsocial;
+        public string cnpj;
+        public string inscricao;
+    }
+}
