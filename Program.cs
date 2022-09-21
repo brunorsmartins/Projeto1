682ddc65155b1ec18cc3c6feb3edff06b48a3d5c
Bruno Martins <brunorsmartins@hotmail.com>
2022-09-20 22:33:24 -0300

    Alterações finais.

--- Program.cs
+++ Program.cs
@@ -1,6 +1,10 @@
 ﻿using Classes;
 
 bool rodando = true;
+pessoafisica pf = new pessoafisica();
+pessoajuridica pj = new pessoajuridica();
+bool vpf = false;
+bool vpj = false;
 
 while (rodando = true){
     Console.WriteLine("1 - Inserir Dados de pessoa física");
@@ -9,35 +13,64 @@ while (rodando = true){
     int menu;
     menu = Int32.Parse(Console.ReadLine());
 
+
     switch(menu)
     {
-        case 1:
-            pessoafisica pf = new pessoafisica();
+        case 1:           
             Console.WriteLine("Digite seu nome");
             pf.nome = Console.ReadLine();
             Console.WriteLine("Digite seu CPF");
             pf.cpf = Console.ReadLine();
             Console.WriteLine("Digite sua idade");
             pf.idade = Int32.Parse(Console.ReadLine());
-            bool vpf = true; // Verififica se o usuário inseriu informações de PF para futura exibição
+            vpf = true; // Verififica se o usuário inseriu informações de PF para futura exibição
         break;
 
         case 2:
-            pessoajuridica pj = new pessoajuridica();
             Console.WriteLine("Digite sua razão social");
             pj.rsocial = Console.ReadLine();
             Console.WriteLine("Digite seu CNPJ");
-            pj.rsocial = Console.ReadLine();
+            pj.cnpj = Console.ReadLine();
             Console.WriteLine("Digite sua Inscrição Municipal");
             pj.inscricao = Console.ReadLine();
-            bool vpj = true;
+            vpj = true;
 
         break;
 
         case 3:
-            Console.WriteLine("asdas" + pf.cpf);
+            if (vpf == true){
+                Console.WriteLine("****PESSOA FÍSICA DETECADA****");
+                Console.WriteLine("Nome: " + pf.nome);
+                Console.WriteLine("CPF: " + pf.cpf);
+                Console.WriteLine("Idade: " + pf.idade);
+
+            }else{
+                
+                Console.WriteLine("****PESSOA FÍSICA NAO DETECADA****");
+            }
+
+            if (vpj == true){
+
+                Console.WriteLine("****PESSOA JURÍDICA DETECADA****");
+                Console.WriteLine("Razão Social: " + pj.rsocial);
+                Console.WriteLine("CNPJ : " + pj.cnpj);
+                Console.WriteLine("Razão Social: " + pj.inscricao);
+
+            }else{
+                Console.WriteLine("****PESSOA JURÍDICA NAO DETECADA****");
+            }
 
 
+        break;
+
+        case 4:
+            Console.WriteLine("DDD");
+            pf.cpf = Console.ReadLine();
+            Console.WriteLine("asdsa" + pf.cpf);
+        break;
+
+        default:
+        Console.WriteLine("Selecione uma opção válida.");
         break;
 
     }
